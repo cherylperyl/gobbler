5a9bd9c85b6f480493cfcd7adddf63950d627ebd
chuenkaiong <chuenkaiong@gmail.com>
2023-03-15 16:43:25 +0800

    changed payment ms to integrate with user management complex ms

--- backend/simple_services/payment/app/schema.py
+++ backend/simple_services/payment/app/schema.py
@@ -1,17 +1,21 @@
+from typing import Optional
 from pydantic import BaseModel
 
 
 class UserBase(BaseModel):
-    stripe_user_id: str = None
-    subscription: str = None
+    userId: Optional[int] = None
+    isPremium: Optional[bool] = False
+    stripeId: Optional[str] = None
+    subscription: Optional[str] = None
 
 
-class UserCreate(UserBase):
-    id: str
+class UserUpdate(UserBase):
+    userId: int
+    isPremium: bool
+    stripeId: str
+    subscription: str
 
 
-class User(UserBase):
-    id: str
-
-    class Config:
-        orm_mode = True
+class CheckoutRequest(BaseModel):
+    userId: int
+    success_url: str
