bb50fc8ce83145adf7e94a932ddb1a89b2444979
chuenkaiong <chuenkaiong@gmail.com>
2023-03-19 20:54:21 +0800

    ignore this i just changed a comment

--- backend/simple_services/payment/app/crud.py
+++ backend/simple_services/payment/app/crud.py
@@ -6,7 +6,7 @@ from . import schema
 USER_MGT_ENDPOINT = os.environ.get("USER_MGT_ENDPOINT") if os.environ.get("USER_MGT_ENDPOINT") is not None else "http://localhost:5005"
 
 def update_user(user: schema.UserUpdate) -> requests.Response:
-    # patch request to user_mgt ms 
+    # patch request to user management ms 
     patch_data = user.dict()
     return requests.patch(
         f"{USER_MGT_ENDPOINT}/user/{user.userId}",
