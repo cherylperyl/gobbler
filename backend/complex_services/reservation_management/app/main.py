662dd2e3e08b4942ec484dda72a214b8fabd3327
developerlxy <esther.lam.2020@business.smu.edu.sg>
2023-03-20 12:30:20 +0800

    solve error for creation

--- backend/complex_services/reservation_management/app/main.py
+++ backend/complex_services/reservation_management/app/main.py
@@ -4,6 +4,7 @@ from fastapi import Depends, FastAPI, HTTPException
 from . import schemas
 from fastapi.responses import JSONResponse
 import os
+import json
 from datetime import datetime
 import requests
 
@@ -60,19 +61,25 @@ def create_reservation(
     reservation.created_at = datetime.now()
     reservation.updated_at = datetime.now()
 
+    post_data = reservation.dict()
+    post_data["created_at"] = json.dumps(reservation.created_at.isoformat(), default=str)[1:-4]+'Z'
+    post_data["updated_at"] = json.dumps(reservation.updated_at.isoformat(), default=str)[1:-4]+'Z'
+
     response = requests.post(
-        f"{reservation_ms_url}/api/reservations",
-        data = reservation
+        f"{reservation_ms_url}",
+        json = post_data
     )
     if response.status_code not in range(200, 300):
         raise HTTPException(response.status_code, detail = response.text)
     
-    return response
+    reservation_data = response.json()
+    
+    return schemas.Reservation.parse_obj(reservation_data)
 
 @app.get("/reservations/all/{user_id}", response_model=List[schemas.Reservation])
 def get_all_posts_reserved_by_user(user_id: int):
     post_ids = requests.get(
-        f"{reservation_ms_url}/api/reservations/{user_id}"
+        f"{reservation_ms_url}/user/{user_id}"
     )
     if post_ids.status_code not in range(200, 300):
         raise HTTPException(post_ids.status_code, detail = post_ids.text)
