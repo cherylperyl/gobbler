d4fa6e12a3e881b4900270bb195275ed538e1ee4
jonathantan1425 <jonathantan1425@gmail.com>
2023-04-01 13:33:54 +0800

    Update scripts to match new folder structure

--- backend/scripts/deploy-kube-mac.sh
+++ backend/scripts/deploy-kube-mac.sh
@@ -4,5 +4,5 @@ set +a
 
 GCP_ENV_VAR_REF='\/${GCP_PROJECT_ID}'
 GCP_PROJECT_ID_DIR="\/$GCP_PROJECT_ID"
-gsed -i "s/$GCP_ENV_VAR_REF/$GCP_PROJECT_ID_DIR/g" ./kubernetes/*
-kubectl apply -f ./kubernetes
+find ./kubernetes -type f -exec gsed -i "s/$GCP_ENV_VAR_REF/$GCP_PROJECT_ID_DIR/g" {} +
+kubectl apply -R -f ./kubernetes
--- backend/scripts/revert-mac.sh
+++ backend/scripts/revert-mac.sh
@@ -4,4 +4,4 @@ set +a
 
 GCP_ENV_VAR_REF="\/$GCP_PROJECT_ID"
 GCP_PROJECT_ID_DIR='\/${GCP_PROJECT_ID}'
-gsed -i "s/$GCP_ENV_VAR_REF/$GCP_PROJECT_ID_DIR/g" ./kubernetes/*
+find ./kubernetes -type f -exec gsed -i "s/$GCP_ENV_VAR_REF/$GCP_PROJECT_ID_DIR/g" {} +
