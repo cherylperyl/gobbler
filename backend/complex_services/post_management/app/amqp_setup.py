3a3efc0e97848e7b12fb45ef4d1c3111cb9ab1b8
jonathantan1425 <jonathantan1425@gmail.com>
2023-04-02 04:24:23 +0800

    Add notifications for reserved users

--- backend/complex_services/post_management/app/amqp_setup.py
+++ backend/complex_services/post_management/app/amqp_setup.py
@@ -1,14 +1,25 @@
 import pika
 
-exchangename = "newposts"
-queue_name = "newposts"
-exchangetype = "topic"
+amqp_configs = {
+    "newpost": {
+        "exchangename": "newpost",
+        "queue_name": "newpost",
+        "exchangetype": "topic",
+    },
+    "updatepost": {
+        "exchangename": "updatepost",
+        "queue_name": "updatepost",
+        "exchangetype": "topic",
+    },
+    "deletepost": {
+        "exchangename": "deletepost",
+        "queue_name": "deletepost",
+        "exchangetype": "topic",
+    },
+}
 
 
 def setup(hostname, port):
-
-    global exchangename, queue_name, exchangetype
-
     # connect to the broker and set up a communication channel in the connection
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(
@@ -18,25 +29,29 @@ def setup(hostname, port):
             # of the connection
             heartbeat=3600,  # when server down
             blocked_connection_timeout=3600,  # when server busy
-        ))
+        )
+    )
 
     channel = connection.channel()
 
     # Set up the exchange if the exchange doesn't exist
-    channel.exchange_declare(
-        exchange=exchangename,
-        exchange_type=exchangetype,  # - use a 'topic' exchange to enable interaction
-        durable=True  # 'durable' makes the exchange survive broker restarts
-    )
+    for _, amqp_config in amqp_configs.items():
+        channel.exchange_declare(
+            exchange=amqp_config["exchangename"],
+            exchange_type=amqp_config[
+                "exchangetype"
+            ],  # - use a 'topic' exchange to enable interaction
+            durable=True,  # 'durable' makes the exchange survive broker restarts
+        )
 
-    # delcare/ create Error queue
-    channel.queue_declare(queue=queue_name, durable=True)  # 'durable' makes the queue survive broker restarts
+        # delcare/ create Error queue
+        channel.queue_declare(queue=amqp_config["queue_name"], durable=True)
 
-    # bind/ add Error queue to exchange
-    channel.queue_bind(
-        exchange=exchangename,
-        queue=queue_name,
-        routing_key='#'  # any routing_key would be matched
-    )
+        # bind/ add Error queue to exchange
+        channel.queue_bind(
+            exchange=amqp_config["exchangename"],
+            queue=amqp_config["queue_name"],
+            routing_key="#",  # any routing_key would be matched
+        )
 
     return channel
