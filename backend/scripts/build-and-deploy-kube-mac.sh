03c2bf8e089671fe8a7d19b9a73dc455461d1edb
jonathantan1425 <jonathantan1425@gmail.com>
2023-04-02 02:03:26 +0800

    Add internal network and redis

--- backend/scripts/build-and-deploy-kube-mac.sh
+++ backend/scripts/build-and-deploy-kube-mac.sh
@@ -2,8 +2,12 @@ set -a # automatically export all variables
 source .env
 set +a
 
+docker compose build
+docker compose push
+
 GCP_ENV_VAR_REF='\/${GCP_PROJECT_ID}'
 GCP_PROJECT_ID_DIR="\/$GCP_PROJECT_ID"
-find ./kubernetes -type f -exec sed -i "s/$GCP_ENV_VAR_REF/$GCP_PROJECT_ID_DIR/g" {} +
+find ./kubernetes -type f -exec gsed -i "s/$GCP_ENV_VAR_REF/$GCP_PROJECT_ID_DIR/g" {} +
 kubectl apply -R -f ./kubernetes
+find ./kubernetes -type f -exec gsed -i "s/$GCP_PROJECT_ID_DIR/$GCP_ENV_VAR_REF/g" {} +
 kubectl rollout restart deployment -n default
