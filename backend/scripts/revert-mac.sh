32baafd95223f67fba201a2b129e7b4ceba6710e
jonathantan1425 <jonathantan1425@gmail.com>
2023-04-01 13:09:08 +0800

    Add kubernetes manifests

---
+++ backend/scripts/revert-mac.sh
@@ -0,0 +1,7 @@
+set -a # automatically export all variables
+source .env
+set +a
+
+GCP_ENV_VAR_REF="\/$GCP_PROJECT_ID"
+GCP_PROJECT_ID_DIR='\/${GCP_PROJECT_ID}'
+gsed -i "s/$GCP_ENV_VAR_REF/$GCP_PROJECT_ID_DIR/g" ./kubernetes/*
