12ebf2d8899b86e5f4c3b00da9d827f3e8ae5561
cherylperyl <cheryl.goh.2020@smu.edu.sg>
2023-05-01 16:00:52 +0800

    add redis caching for reservation simple ms with fastapi_redis_cache - but does not work for objects ://

--- backend/simple_services/reservation/app/main.py
+++ backend/simple_services/reservation/app/main.py
@@ -1,12 +1,15 @@
 from typing import List
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Request, Response
 from sqlalchemy.orm import Session
 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
 
 from fastapi.responses import JSONResponse
+from fastapi_redis_cache import FastApiRedisCache, cache_one_minute
+from fastapi.encoders import jsonable_encoder
+import os
 
 
 ########### DO NOT MODIFY BELOW THIS LINE ###########
@@ -33,32 +36,57 @@ def get_db():
     finally:
         db.close()
 
-
 ########### DO NOT MODIFY ABOVE THIS LINE ###########
 
 
+@app.on_event("startup")
+def startup():
+    """
+    Initialize Redis
+    """
+    redis_cache = FastApiRedisCache()
+    RESERVATION_REDIS_SERVER = os.environ.get("RESERVATION_REDIS_SERVER", "host.docker.internal")
+    RESERVATION_REDIS_PORT = os.environ.get("RESERVATION_REDIS_PORT", "6379")
+    redis_cache.init(
+        host_url=f"redis://{RESERVATION_REDIS_SERVER}:{RESERVATION_REDIS_PORT}",
+        prefix="myapi-cache",
+        response_header="X-MyAPI-Cache",
+        ignore_arg_types=[Request, Response, Session]
+    )
+
+
 @app.get("/ping")
-def ping():
+@cache_one_minute()
+async def ping(
+    response: Response
+):
     """
     Health check endpoint
     """
-    return {"ping": "pong!"}
+    return {"ping": "pong"}
 
 
 @app.get("/reservations/all", response_model=List[schemas.Reservation])
-def get_all_reservations(db: Session = Depends(get_db)):
+@cache_one_minute()
+def get_all_reservations(
+    response: Response,
+    db: Session = Depends(get_db)
+):
     """
     Get all reservations
     """
     reservations = crud.get_all(db)
     if not reservations:
         raise HTTPException(status_code=404, detail="No reservations found")
-    return reservations
+    return jsonable_encoder(reservations)
 
 
 @app.get("/reservations/{reservation_id}", response_model=schemas.Reservation)
+@cache_one_minute()
 def get_reservation_by_reservation_id(
-    reservation_id: int, db: Session = Depends(get_db)
+    response: Response,
+    reservation_id: int,
+    db: Session = Depends(get_db)
 ):
     """
     Get reservation by reservation_id
@@ -70,7 +98,12 @@ def get_reservation_by_reservation_id(
 
 
 @app.get("/reservations/post/{post_id}", response_model=List[schemas.Reservation])
-def get_reservations_by_post_id(post_id: int, db: Session = Depends(get_db)):
+@cache_one_minute()
+def get_reservations_by_post_id(
+    response: Response,
+    post_id: int,
+    db: Session = Depends(get_db)
+):
     """
     Get reservations by post_id
     """
@@ -80,7 +113,12 @@ def get_reservations_by_post_id(post_id: int, db: Session = Depends(get_db)):
     return reservations
 
 @app.get("/reservations/user/{user_id}", response_model=List[schemas.Reservation])
-def get_reservations_by_user_id(user_id: int, db: Session = Depends(get_db)):
+@cache_one_minute()
+def get_reservations_by_user_id(
+    response: Response,
+    user_id: int,
+    db: Session = Depends(get_db)
+):
     """
     Get reservations by user_id
     """
@@ -90,7 +128,12 @@ def get_reservations_by_user_id(user_id: int, db: Session = Depends(get_db)):
     return reservations
 
 @app.get("/reservations/post/slots/{post_id}")
-def get_reservation_count_by_post_id(post_id: int, db: Session = Depends(get_db)):
+@cache_one_minute()
+def get_reservation_count_by_post_id(
+    response: Response,
+    post_id: int,
+    db: Session = Depends(get_db)
+):
     """
     Get number of reservations by post_id
     """
@@ -101,7 +144,12 @@ def get_reservation_count_by_post_id(post_id: int, db: Session = Depends(get_db)
 
 
 @app.get("/reservations/posts/slots")
-def get_reservations_by_list_of_post_id(post_id_list: List[int], db: Session = Depends(get_db)):
+@cache_one_minute()
+def get_reservations_by_list_of_post_id(
+    response: Response,
+    post_id_list: List[int],
+    db: Session = Depends(get_db)
+):
     """
     Get number of reservations by a list of post_ids
     """
@@ -118,7 +166,13 @@ def get_reservations_by_list_of_post_id(post_id_list: List[int], db: Session = D
 
 
 @app.get("/reservations/user/{user_id}/post/{post_id}", response_model=schemas.Reservation)
-def get_reservation_by_user_id_and_post_id(user_id: int, post_id: int, db: Session = Depends(get_db)):
+@cache_one_minute()
+def get_reservation_by_user_id_and_post_id(
+    response: Response,
+    user_id: int,
+    post_id: int,
+    db: Session = Depends(get_db)
+):
     """
     Get reservation by user_id and post_id
     """
